f5417af036fbd9d557520339869bf9d873ea319b
clowwindy <clowwindy42@gmail.com>
2014-06-22 16:17:25 +0800

    lint code

--- chinadns/dnsrelay.py
+++ chinadns/dnsrelay.py
@@ -269,8 +269,6 @@ class TCPDNSRelay(DNSRelay):
 
 
 def main():
-
-
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s %(levelname)-8s %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S', filemode='a+')
